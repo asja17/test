db45f88254922c5d23a383ea1afa2c5159248f2b
Asim Jarrar <asim@kth.se>
2022-04-01 10:53:37 +0200

    first commit

---
+++ 32 For loops.py
@@ -0,0 +1,12 @@
+#This is the complete for loop program from page 32
+
+
+banan = input("Skriv ett ord ")
+antal = int(input("Ange hur många gånger ordet ska skrivas: "))
+for x in range(0,antal):
+    print (banan)
+    print(x)
+
+
+
+    
