1776129d01ab9775caf08725516eb391917834c1
Asim Jarrar <asim@kth.se>
2022-04-01 10:55:09 +0200

    changed

--- 32 For loops.py
+++ 32 For loops.py
@@ -1,7 +1,7 @@
 #This is the complete for loop program from page 32
 
 
-banan = input("Skriv ett ord ")
+banan = input("Skriv ett ord changed")
 antal = int(input("Ange hur många gånger ordet ska skrivas: "))
 for x in range(0,antal):
     print (banan)
